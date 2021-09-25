b008f6388b3f07a3a9484b39abcddf9ed5d36f2c
karolined1 <91218083+karolined1@users.noreply.github.com>
2021-09-25 13:07:11 +0200

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,12 +4,16 @@ let hinder = false
 basic.forever(function () {
     if (hinder == false) {
         bitbot.setLedColor(0x00FF00)
-        bitbot.goms(BBDirection.Forward, 100, 20)
+        bitbot.goms(BBDirection.Forward, 100, 12)
         while (bitbot.readLine(BBLineSensor.Left) == 1 && bitbot.readLine(BBLineSensor.Right) == 0) {
-            bitbot.rotatems(BBRobotDirection.Left, 60, 9)
+            bitbot.rotatems(BBRobotDirection.Left, 60, 10)
         }
         while (bitbot.readLine(BBLineSensor.Left) == 0 && bitbot.readLine(BBLineSensor.Right) == 1) {
-            bitbot.rotatems(BBRobotDirection.Right, 60, 9)
+            bitbot.rotatems(BBRobotDirection.Right, 60, 10)
+            while (bitbot.readLine(BBLineSensor.Left) == 1 && bitbot.readLine(BBLineSensor.Right) == 1) {
+                bitbot.stop(BBStopMode.Coast)
+                music.playTone(494, music.beat(BeatFraction.Quarter))
+            }
         }
         if (bitbot.sonar(BBPingUnit.Centimeters) < 5) {
             bitbot.stop(BBStopMode.Coast)
@@ -18,11 +22,19 @@ basic.forever(function () {
             hinder = true
         }
     } else if (hinder == true) {
-        bitbot.rotatems(BBRobotDirection.Right, 40, 700)
-        bitbot.goms(BBDirection.Forward, 40, 700)
-        bitbot.rotatems(BBRobotDirection.Left, 40, 700)
-        bitbot.goms(BBDirection.Forward, 100, 20)
+        bitbot.rotatems(BBRobotDirection.Right, 40, 350)
+        basic.pause(500)
+        bitbot.goms(BBDirection.Forward, 40, 600)
+        basic.pause(500)
+        bitbot.rotatems(BBRobotDirection.Left, 40, 300)
+        basic.pause(500)
+        bitbot.goms(BBDirection.Forward, 40, 600)
+        basic.pause(500)
+        bitbot.rotatems(BBRobotDirection.Left, 40, 300)
+        basic.pause(500)
+        bitbot.goms(BBDirection.Forward, 40, 800)
+        basic.pause(500)
+        bitbot.rotatems(BBRobotDirection.Right, 40, 200)
         hinder = false
-        bitbot.rotatems(BBRobotDirection.Left, 40, 700)
     }
 })
